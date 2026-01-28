78ee244325f5ef9cdb4efd815295164b3ef281ea
jonihalimsentrawebmedia <joni.halim@sentrawebmedia.com>
2026-01-28 15:10:26 +0700

    fi done

--- src/provider/axios.tsx
+++ src/provider/axios.tsx
@@ -18,7 +18,7 @@ AxiosClient.interceptors.request.use(
     const host = window.location.host
     const origin =
       host === 'localhost:3000' || host === '192.168.20.83:3000' || 'landing-prodi.vercel.app'
-        ? 'tradis-bi.stain-madina.ac.id'
+        ? 'stain-madina.ac.id'
         : window.location.origin
 
     const headers = new AxiosHeaders({
