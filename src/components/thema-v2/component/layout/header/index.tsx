7853f1b0a8f2d7710ad11d30b5a3ada448d66252
joni Halim <joni.halim@sentrawebmedia.com>
2026-04-13 07:07:16 +0700

    fix title white space preline

--- src/components/thema-v2/component/layout/header/index.tsx
+++ src/components/thema-v2/component/layout/header/index.tsx
@@ -53,7 +53,7 @@ export const HeaderLayout = () => {
             className={'size-[100px] w-[75px] h-[75px]'}
           />
           <div className={'flex flex-col gap-y-1.5'}>
-            <h1 className="lg:text-2xl max-w-[450px] font-semibold text-primary">
+            <h1 className="lg:text-2xl max-w-[450px] font-semibold text-primary whitespace-pre-line">
               {detail?.nama} ({detail?.kode_jenjang})
             </h1>
             <p className={'text-[10px]'}>{detail?.nama_parent_satuan_organisasi}</p>
